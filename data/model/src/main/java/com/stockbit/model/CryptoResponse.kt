4fc8e58914a6bb9e939b42ba438c638519006e2d
Alvin Tandiardi <alvintandiardi@gmail.com>
2021-10-02 10:57:55 +0700

    Menambahkan Model CryptoResponse

---
+++ data/model/src/main/java/com/stockbit/model/CryptoResponse.kt
@@ -0,0 +1,52 @@
+package com.stockbit.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CryptoResponse(
+    @SerializedName("Type")
+    val type: Int? = null,
+    @SerializedName("Message")
+    val message: String? = null,
+    @SerializedName("MetaData")
+    val metaData: MetaData? = null,
+    @SerializedName("HasWarning")
+    val hasWarning: Boolean? = null,
+    @SerializedName("Data")
+    val data: List<DataItem?>? = null,
+)
+
+data class DataItem(
+    @SerializedName("DISPLAY")
+    val display: Display? = null,
+
+    @SerializedName("CoinInfo")
+    val coinInfo: CoinInfo? = null
+)
+
+data class CoinInfo(
+    @SerializedName("Id")
+    val id: String? = null,
+
+    @SerializedName("Name")
+    val name: String? = null,
+
+    @SerializedName("FullName")
+    val fullName: String? = null,
+)
+
+
+data class MetaData(
+    @SerializedName("Count")
+    val count: Int? = null
+)
+
+
+data class Display(
+    @SerializedName("USD")
+    val usd: USD? = null
+)
+
+data class USD(
+    @SerializedName("PRICE")
+    val price: String? = null
+)
